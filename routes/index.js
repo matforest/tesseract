35ccb0545f375dd8cc7c7ce50c72f0f84561ada8
Ben Petito <benpetito@gmail.com>
2013-05-31 20:44:32 +0930

    adding foundation for gui
    added modernizr, foundation, zepto, etc

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@
 /*
  * GET home page.
  */
-
 exports.index = function(req, res){
-  res.render('index', { title: 'Tessaract' });
+  res.render('index', { 
+    title: 'Tessaract',
+    subtitle: 'Doing awesum stuff with gov\'t data'
+  });
 };
