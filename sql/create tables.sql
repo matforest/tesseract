7ce0d140cbec9a72eb7bfa4dee9476a206b51c1e
Ben Petito <benpetito@gmail.com>
2013-06-01 12:11:05 +0930

    added adult community create table sql

--- sql/create tables.sql
+++ sql/create tables.sql
@@ -1,9 +1,18 @@
 
 -- Master table
-
-
-
 create table master (
 the_geom geometry,
 id 
-)
+);
+
+-- Adult Community Education Providers table
+create table ace (
+  provider_name varchar,
+  region varchar,
+  phone varchar,
+  email varchar,
+  contact varchar,
+  address varchar,
+  latitude double precision,
+  longitude double precision
+);
