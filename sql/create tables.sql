21f93c5a76e4a24e1b1e4be666e68524596a5086
Ben Petito <benpetito@gmail.com>
2013-06-01 12:20:44 +0930

    updated create table

--- sql/create tables.sql
+++ sql/create tables.sql
@@ -6,7 +6,7 @@ id
 );
 
 -- Adult Community Education Providers table
-create table if not exists ace (
+create table if not exists gis_schema.ace (
   provider_name varchar,
   region varchar,
   phone varchar,
@@ -16,3 +16,11 @@ create table if not exists ace (
   latitude double precision,
   longitude double precision
 );
+
+--COPY gis_schema.ace FROM '/path/to/csv/dfeest-listed-adult-community-education-providers-2012-13.csv' DELIMITER ',' CSV;
+
+alter table gis_schema.ace
+  add column geometry geometry(POINT, 4326);
+
+UPDATE gis_schema.ace 
+  SET geometry = ST_SetSRID(ST_MakePoint(longitude,latitude),4326);
